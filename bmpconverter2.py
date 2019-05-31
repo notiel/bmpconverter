ec93f941a7875c34b91b05761926e8c00bfaf8fb
Julia Salnikova (Notiel) <agereth@gmail.com>
2019-05-31 18:44:32 +0300

    protocol sending changed

--- bmpconverter2.py
+++ bmpconverter2.py
@@ -16,29 +16,30 @@ def main(folder: str, comport: str, delay: int, depth: int):
             for file in os.listdir(folder):
                 if file.lower().endswith('.bmp'):
                     data = Image.open(os.path.join(folder, file))
-                    pixels = data.getdata()
-                    command = bytes()
-                    for i in range (data.size[1]):
-                        for j in range(data.size[0]):
-                            r, g, b = pixels[i*data.size[0] + j]
+                    w, h = data.size
+                    command = ":30"
+                    for i in range (w):
+                        for j in range(h):
+                            r, g, b  = data.getpixel((i, j))
                             #print(r, g, b)
                             if depth != 8:
                                 r = r >> (8 - depth)
                                 g = g >> (8 - depth)
                                 b = b >> (8 - depth)
-                            command += r.to_bytes(1, byteorder='big')
-                            command += g.to_bytes(1, byteorder='big')
-                            command += b.to_bytes(1, byteorder='big')
+                            command += r.to_bytes(1, byteorder='big').hex()
+                            command += g.to_bytes(1, byteorder='big').hex()
+                            command += b.to_bytes(1, byteorder='big').hex()
 
-                    ser.write(command)
-                    #print(command)
+                    command+='\r\n'
+                    ser.write(bytes(command, encoding='utf-8'))
+                    print(command)
                     time.sleep(0.001*delay)
     finally:
         ser.close()
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        if len(sys.argv) > 5:
+        if len(sys.argv) >= 5:
             main(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
         else:
             print("Недостаточно параметров, ожидаемыее параметры: имя папки, номер компорта, таймаут в мс, глубина цвета")
